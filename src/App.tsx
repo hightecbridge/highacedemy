558933c48f31ed0d0843ec740e51b13b7e2cc547
DLIVE <apporty@gmail.com>
2026-03-09 16:13:12 +0900

    first commit2

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,26 @@
-import { useState } from "react";
-
-type members = {
-  id: number;
-  name: string;
-  email: string;
-};
+import { useEffect, useState } from "react"
 
 function App() {
-  const [members, setMembers] = useState([]);
 
-  const loadMembers = async () => {
-    try {
-      const res = await fetch("http://13.209.114.196:8080/api/members");
+  const [members, setMembers] = useState([])
 
-      if (!res.ok) {
-        throw new Error("API 오류: " + res.status);
-      }
-
-      const data = await res.json();
-      setMembers(data);
-
-    } catch (error) {
-      console.error("API 호출 실패:", error);
-    }
-  };
+  useEffect(() => {
+    fetch("http://13.209.114.196:8080/api/members")
+      .then(res => res.json())
+      .then(data => setMembers(data))
+  }, [])
 
   return (
     <div>
-      <h2>회원 목록</h2>
+      <h1>Member List</h1>
 
-      <button onClick={loadMembers}>
-        리스트 불러오기2
-      </button>
-
-      <ul>
-        {members.map((m) => (
-          <li key={m.id}>
-            {m.name} ({m.email})
-          </li>
-        ))}
-      </ul>
-
+      {members.map((m:any) => (
+        <div key={m.id}>
+          {m.name}
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
